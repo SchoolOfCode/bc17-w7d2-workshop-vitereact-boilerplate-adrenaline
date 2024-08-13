b1a2db75efa4408816a523a54a0d6945eb604c09
habib <habibali15@hotmail.com>
2024-08-13 12:32:40 +0100

    created header component and pulled accross

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components'
+import Header from './components/header/header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/header/header.jsx
@@ -0,0 +1,4 @@
+export default function Header() {
+    return <header>🔥Fireplace Palace</header>;
+  }
+  
